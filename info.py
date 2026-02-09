845bc7be426801f4fa4bb148c614bd7d3cbd9475
movies4u1994 <shelkeanant14@gmail.com>
2026-02-09 08:20:28 +0530

    Update info.py

--- info.py
+++ info.py
@@ -124,7 +124,7 @@ THREE_VERIFY_GAP = int(environ.get('THREE_VERIFY_GAP', "54000"))
 # ============================
 GRP_LNK = environ.get('GRP_LNK', 'https://t.me/movies4uhd2') # Group link for the bot
 OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/Movies4uhd14') # Owner link for the bot
-UPDATE_CHNL_LNK = environ.get('UPDATE_CHNL_LNK', 'https://t.me/MovieHuntZone') # Update channel link for the bot
+UPDATE_CHNL_LNK = environ.get('UPDATE_CHNL_LNK', 'https://t.me/movies4uhd123') # Update channel link for the bot
 
 # ============================
 # User Configuration
