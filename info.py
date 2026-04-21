91a2188fc3b93955eded3fc7d5476e30192d2a24
movies4u1994 <shelkeanant14@gmail.com>
2026-04-21 19:53:24 +0530

    Update BOT_TOKEN default value to empty string
    Change default BOT_TOKEN to an empty string for security.

--- info.py
+++ info.py
@@ -20,7 +20,7 @@ def is_enabled(value, default):
 SESSION = environ.get('SESSION', 'dreamxbotz_search')   # Session name for the bot
 API_ID = int(environ.get('API_ID', '29761749')) # API ID from my.telegram.org
 API_HASH = environ.get('API_HASH', 'a18b40e995d5b74ec633a0cfdfa8e175')  # API Hash from my.telegram.org
-BOT_TOKEN = environ.get('BOT_TOKEN', "8307939018:AAFhW0Wxc01TuKcKRDAoLuJhiqyTTL2RE_A")    # Bot token from @BotFather
+BOT_TOKEN = environ.get('BOT_TOKEN', "")    # Bot token from @BotFather
 
 # ============================
 # Bot Settings Configuration
