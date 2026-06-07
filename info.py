f25e2ef5cb6c611a22d220c29a249cb7c06a614f
movies4u1994 <shelkeanant14@gmail.com>
2026-06-07 13:38:18 +0530

    Update BOT_TOKEN in info.py
    Update bot token

--- info.py
+++ info.py
@@ -20,7 +20,7 @@ def is_enabled(value, default):
 SESSION = environ.get('SESSION', 'dreamxbotz_search')   # Session name for the bot
 API_ID = int(environ.get('API_ID', '29761749')) # API ID from my.telegram.org
 API_HASH = environ.get('API_HASH', 'a18b40e995d5b74ec633a0cfdfa8e175')  # API Hash from my.telegram.org
-BOT_TOKEN = environ.get('BOT_TOKEN', "7833968452:AAELph78j7mTcz4Hcqqh6HDCQQAzkQ7XwVw")    # Bot token from @BotFather
+BOT_TOKEN = environ.get('BOT_TOKEN', "7833968452:AAHJaqW2zP-IUCrhBZDDXhcerDoSyGLTv3A")    # Bot token from @BotFather
 
 # ============================
 # Bot Settings Configuration
