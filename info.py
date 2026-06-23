e16d06b84b009a9cb82be477f995ea74e105100b
movies4u1994 <shelkeanant14@gmail.com>
2026-06-24 02:11:15 +0530

    Update premium plan durations in info.py
    Update premium plan durations in info.py

--- info.py
+++ info.py
@@ -65,8 +65,8 @@ QR_CODE = environ.get('QR_CODE', 'https://files.catbox.moe/kk2ltl.jpg')    # QR 
 OWNER_UPI_ID = environ.get('OWNER_UPI_ID', 'Shelkeanant14@okicici')    # Owner UPI ID for payments
 
 STAR_PREMIUM_PLANS = {
-    15: "20day",
-    21: "1month",    
+    15: "17day",
+    21: "2month",    
     40: "4month", 
     75: "8month",
     110: "1year",
